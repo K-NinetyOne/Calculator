6ed074a1899557651fe392a1e32a769aac1f5a86
K-NinetyOne <121988014+K-NinetyOne@users.noreply.github.com>
2026-05-09 00:01:30 +0100

    Accepts operations on answers. Added function to call on click of all numBtns

--- calculator.js
+++ calculator.js
@@ -3,6 +3,18 @@ let equationOperation = [];
 let operator;
 let answer;
 
+const numberBtns = document.querySelectorAll(".numberBtn");
+
+numberBtns.forEach((numBtn) => {
+  numBtn.addEventListener("click", function () {
+    equationInput.push(Number(numBtn.value));
+    document.querySelector("#screen").innerText = equationInput.join("");
+    console.log(equationInput);
+    console.log(document.querySelector(".numberBtn").value);
+    console.log(document.querySelector(".numberBtn").value);
+  });
+});
+
 const addItems = function (arr) {
   return arr.reduce((item1, item2) => item1 + item2, 0);
 };
@@ -19,24 +31,6 @@ const divideItems = function (arr) {
   return arr.reduce((item1, item2) => item1 / item2);
 };
 
-/*const sum = function (arr) {
-  return arr.reduce((total, currentItem) => total + currentItem, 0);
-};
-
-const product = function (arr) {
-  return arr.reduce((total, currentItem) => total * currentItem);
-};*/
-
-const num1 = document.querySelector("#btn1");
-const num2 = document.querySelector("#btn2");
-const num3 = document.querySelector("#btn3");
-const num4 = document.querySelector("#btn4");
-const num5 = document.querySelector("#btn5");
-const num6 = document.querySelector("#btn6");
-const num7 = document.querySelector("#btn7");
-const num8 = document.querySelector("#btn8");
-const num9 = document.querySelector("#btn9");
-const num0 = document.querySelector("#btn0");
 const divide = document.querySelector("#divideBtn");
 const multiply = document.querySelector("#multiplyBtn");
 const plus = document.querySelector("#plusBtn");
@@ -44,56 +38,6 @@ const minus = document.querySelector("#minusBtn");
 const equals = document.querySelector("#equalsBtn");
 const clearAll = document.querySelector("#clearBtn");
 
-num1.addEventListener("click", function () {
-  equationInput.push(1);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num2.addEventListener("click", function () {
-  equationInput.push(2);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num3.addEventListener("click", function () {
-  equationInput.push(3);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num4.addEventListener("click", function () {
-  equationInput.push(4);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num5.addEventListener("click", function () {
-  equationInput.push(5);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num6.addEventListener("click", function () {
-  equationInput.push(6);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num7.addEventListener("click", function () {
-  equationInput.push(7);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num8.addEventListener("click", function () {
-  equationInput.push(8);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num9.addEventListener("click", function () {
-  equationInput.push(9);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
-num0.addEventListener("click", function () {
-  equationInput.push(0);
-  document.querySelector("#screen").innerText = equationInput.join("");
-  console.log(equationInput);
-});
 divide.addEventListener("click", function () {
   document.querySelector("#screen").innerText = "÷";
   operator = "divide";
@@ -131,6 +75,8 @@ equals.addEventListener("click", function () {
   } else if (operator === "divide") {
     answer = divideItems(equationOperation);
   }
+  equationInput = [];
+  equationInput[0] = answer;
   console.log(answer);
   document.querySelector("#screen").innerText = answer;
 });
@@ -140,3 +86,11 @@ clearAll.addEventListener("click", function () {
   equationInput = [];
   equationOperation = [];
 });
+
+/*Next steps: 
+Apply DRY to event listeners and functions that are repeated. 
+Expand operator array (push each  to array instead of specifying its locaiton [0], [1] etc).
+  This will allow reduce to work on any  of operands.
+Make equals clear the old equationInput array and assign its output value (answer) to the equationInput
+  array[0], then another operator will log it to equationOperation and allow a second operand to be added
+  */
