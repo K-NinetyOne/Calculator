dd0f89410cdc4eedbd1fa8f464c549a390e1a00d
K-NinetyOne <121988014+K-NinetyOne@users.noreply.github.com>
2026-05-09 23:37:47 +0100

    Cleaned up code by writing functions using DRY principles

--- calculator.js
+++ calculator.js
@@ -3,94 +3,80 @@ let equationOperation = [];
 let operator;
 let answer;
 
+const screen = document.querySelector("#screen");
+
+//Operands--------------------------------------------------------------------
+
 const numberBtns = document.querySelectorAll(".numberBtn");
 
-numberBtns.forEach((numBtn) => {
+numberBtns.forEach(function (numBtn) {
   numBtn.addEventListener("click", function () {
     equationInput.push(Number(numBtn.value));
-    document.querySelector("#screen").innerText = equationInput.join("");
-    console.log(equationInput);
-    console.log(document.querySelector(".numberBtn").value);
-    console.log(document.querySelector(".numberBtn").value);
+    screen.innerText = equationInput.join("");
   });
 });
 
+//Operators-------------------------------------------------------------------
+
+const operatorBtns = document.querySelectorAll(".operatorBtn");
+
+operatorBtns.forEach(function (opBtn) {
+  opBtn.addEventListener("click", function () {
+    screen.innerText = opBtn.value;
+    operator = opBtn.value;
+    equationOperation[0] = Number(equationInput.join(""));
+    equationInput = [];
+  });
+});
+
+//Divide
+const divideItems = function (arr) {
+  return arr.reduce((item1, item2) => item1 / item2);
+};
+//Multiply
+const multiplyItems = function (arr) {
+  return arr.reduce((item1, item2) => item1 * item2);
+};
+//Add
 const addItems = function (arr) {
   return arr.reduce((item1, item2) => item1 + item2, 0);
 };
-
+//Minus
 const subtractItems = function (arr) {
   return arr.reduce((item1, item2) => item1 - item2);
 };
 
-const multiplyItems = function (arr) {
-  return arr.reduce((item1, item2) => item1 * item2);
-};
-
-const divideItems = function (arr) {
-  return arr.reduce((item1, item2) => item1 / item2);
-};
-
-const divide = document.querySelector("#divideBtn");
-const multiply = document.querySelector("#multiplyBtn");
-const plus = document.querySelector("#plusBtn");
-const minus = document.querySelector("#minusBtn");
-const equals = document.querySelector("#equalsBtn");
-const clearAll = document.querySelector("#clearBtn");
-
-divide.addEventListener("click", function () {
-  document.querySelector("#screen").innerText = "÷";
-  operator = "divide";
-  equationOperation[0] = Number(equationInput.join(""));
-  equationInput = [];
-  console.log(equationOperation);
-  console.log(equationInput.join(""));
-});
-multiply.addEventListener("click", function () {
-  document.querySelector("#screen").innerText = "x";
-  operator = "multiply";
-  equationOperation[0] = Number(equationInput.join(""));
-  equationInput = [];
-});
-plus.addEventListener("click", function () {
-  document.querySelector("#screen").innerText = "+";
-  operator = "plus";
-  equationOperation[0] = Number(equationInput.join(""));
-  equationInput = [];
-});
-minus.addEventListener("click", function () {
-  document.querySelector("#screen").innerText = "-";
-  operator = "minus";
-  equationOperation[0] = Number(equationInput.join(""));
-  equationInput = [];
-});
-equals.addEventListener("click", function () {
+//Equals
+document.querySelector("#equalsBtn").addEventListener("click", function () {
   equationOperation[1] = Number(equationInput.join(""));
-  if (operator === "minus") {
+  if (operator === "-") {
     answer = subtractItems(equationOperation);
-  } else if (operator === "plus") {
+  } else if (operator === "+") {
     answer = addItems(equationOperation);
-  } else if (operator === "multiply") {
+  } else if (operator === "x") {
     answer = multiplyItems(equationOperation);
-  } else if (operator === "divide") {
-    answer = divideItems(equationOperation);
+  } else if (operator === "÷") {
+    if (equationOperation[1] === 0) {
+      answer = "Can't divide by 0";
+    } else {
+      answer = divideItems(equationOperation);
+    }
   }
   equationInput = [];
   equationInput[0] = answer;
-  console.log(answer);
-  document.querySelector("#screen").innerText = answer;
+  screen.innerText = answer;
 });
-clearAll.addEventListener("click", function () {
-  console.log("CLEARED");
-  document.querySelector("#screen").innerText = "";
+
+//Clear
+document.querySelector("#clearBtn").addEventListener("click", function () {
+  screen.innerText = "";
   equationInput = [];
   equationOperation = [];
 });
 
 /*Next steps: 
-Apply DRY to event listeners and functions that are repeated. 
 Expand operator array (push each  to array instead of specifying its locaiton [0], [1] etc).
-  This will allow reduce to work on any  of operands.
-Make equals clear the old equationInput array and assign its output value (answer) to the equationInput
-  array[0], then another operator will log it to equationOperation and allow a second operand to be added
+  This will allow reduce to work on any number of operands later (outside of project scope currently)
+Make condition in the operatorBtn eventListener that if array[0] is null then ignore and use - for number
+//screen.innerText = answer; then clear screen to avoid apendng new number to answer
   */
